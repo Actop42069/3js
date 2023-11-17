3b002a33ffc7f1128dff614117dba0a90b8c7fbd
Achyut <agaihr10@gmail.com>
2023-11-17 16:51:10 +0545

    unsaved changes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import {ObritControls, OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'dat.gui';
 
 const renderer = new THREE.WebGLRenderer();
 
@@ -9,15 +11,70 @@ document.body.appendChild(renderer.domElement);
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(
-    75,
+    45,
     window.innerWidth / innerHeight,
     0.1,
     1000
 );
 
-const axesHelper = new THREE.AxesHelper(5);
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+const axesHelper = new THREE.AxesHelper(3);
 scene.add(axesHelper);
 
-camera.position.z = 5;
+
+camera.position.set(-10,30,30);
+orbit.update();
+
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({color : 0x00DA05});
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
+
+const planeGeometry = new THREE.PlaneGeometry(30,30);
+const planeMaterial = new THREE.MeshBasicMaterial({
+    color: 0xFFFFFF,
+    side:THREE.DoubleSide
+});
+const plane = new THREE.Mesh(planeGeometry,planeMaterial);
+scene.add(plane);
+plane.rotation.x = -0.5* Math.PI;
+
+const gridHelper = new THREE.GridHelper(30);
+scene.add(gridHelper);
+
+const sphereGeometry = new THREE.SphereGeometry(4, 50, 50);
+const sphereMaterial = new THREE.MeshBasicMaterial({
+    color: 0x0000FF,
+    wireframe: false
+});
+const sphere = new THREE.Mesh(sphereGeometry,sphereMaterial);
+scene.add(sphere);
+
+sphere.position.set(-10,10,0);
+
+const gui = new dat.GUI();
+
+const options = {
+    sphereColor : '#ffea00',
+    wireframe: false
+}
+
+gui.addColor(options, 'sphereColor').onChange(function(e){
+    sphere.material.color.set(e)
+});
+
+gui.add(options, 'wireframe').onChange(function(e){
+    sphere.material.wireframe = e;
+});
+
+function animate(time){
+    box.rotation.x = time/1000;
+    box.rotation.y = time/1000;
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
 
 renderer.render(scene, camera);
